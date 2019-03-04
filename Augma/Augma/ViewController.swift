496325e2875d9fd6c5a5bf0f217fc502d1c0c7fd
Chase Carnaroli <chasecarnaroli@gmail.com>
2019-03-03 21:43:31 -0800

    Add ios file

---
+++ Augma/Augma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Augma
+//
+//  Created by Chase Carnaroli on 3/3/19.
+//  Copyright © 2019 Chase Carnaroli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
