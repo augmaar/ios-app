81bba8992f8155f112bdb0ddd44a69b7295c1315
mtwynn <tammn4@uci.edu>
2019-03-10 18:58:56 -0700

    Implemented search functionality

---
+++ Augma/Augma/View Controllers/PictureGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  PictureGridCell.swift
+//  Augma
+//
+//  Created by Tam Nguyen on 3/10/19.
+//  Copyright © 2019 Chase Carnaroli. All rights reserved.
+//
+
+import UIKit
+
+class PictureGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pictureView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+}
--- Augma/Augma/View Controllers/SearchController.swift
+++ Augma/Augma/View Controllers/SearchController.swift
@@ -7,28 +7,94 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class SearchController: UIViewController {
-
+class SearchController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate {
+    
+    
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var collectionView: UICollectionView!
     
     
+    var artwork = [[String:Any]]()
+    var filteredArtwork = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        searchBar.delegate = self
+        
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4 // Space in between ROWS
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 2
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                self.artwork = dataDictionary["results"] as! [[String:Any]]
+                
+                self.filteredArtwork = self.artwork
+                
+                self.collectionView.reloadData()
+                
+            }
+        }
+        task.resume()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filteredArtwork.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureGridCell", for: indexPath) as! PictureGridCell
+        
+        let movie = filteredArtwork[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.pictureView.af_setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredArtwork = searchText.isEmpty ? artwork : artwork.filter { (piece: [String:Any]) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            let title = piece["original_title"] as! String
+            return title.lowercased().contains(searchText.lowercased())
+        }
+        
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(filteredArtwork[indexPath.item]["original_title"]!)
+    }
+    
+    
 }
