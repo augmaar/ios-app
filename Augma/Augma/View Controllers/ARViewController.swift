144f41425d62962a13d16cbca80491d338737d1c
Rodrigo Leyva <rodrigoleyva@Rodrigos-MacBook-Air.local>
2019-03-10 17:49:41 -0700

    Added the ARSceneView added the grid class file also made changes in the ARViewController file added the feature to scane vertical planes and a gesture recognizer to pin paintings onto the plane that is recongized as a grid

--- Augma/Augma/View Controllers/ARViewController.swift
+++ Augma/Augma/View Controllers/ARViewController.swift
@@ -7,13 +7,107 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
-class ARViewController: UIViewController {
+class ARViewController: UIViewController,ARSCNViewDelegate {
 
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    var grids = [Grid]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // show debug info/feature points
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        // Create a new scene
+        let scene = SCNScene()
+        // Set the scene to the view
+        sceneView.scene = scene
 
         // Do any additional setup after loading the view.
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        sceneView.addGestureRecognizer(gestureRecognizer)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .vertical
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // When plane is scanned a ready a grid will appear and user can add painting on the tapped gesture
+    @objc func tapped(gesture: UITapGestureRecognizer) {
+        // Get 2D position of touch event on screen
+        let touchPosition = gesture.location(in: sceneView)
+        
+        // Translate those 2D points to 3D points using hitTest (existing plane)
+        let hitTestResults = sceneView.hitTest(touchPosition, types: .existingPlaneUsingExtent)
+        
+        // Get hitTest results and ensure that the hitTest corresponds to a grid that has been placed on a wall
+        guard let hitTest = hitTestResults.first, let anchor = hitTest.anchor as? ARPlaneAnchor, let gridIndex = grids.index(where: { $0.anchor == anchor }) else {
+            return
+        }
+        addPainting(hitTest, grids[gridIndex])
+    }
+    func addPainting(_ hitResult: ARHitTestResult, _ grid: Grid) {
+        // 1.
+        let planeGeometry = SCNPlane(width: 0.2, height: 0.35)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "sample-mona-lisa")
+        planeGeometry.materials = [material]
+        
+        // 2.
+        let paintingNode = SCNNode(geometry: planeGeometry)
+        paintingNode.transform = SCNMatrix4(hitResult.anchor!.transform)
+        paintingNode.eulerAngles = SCNVector3(paintingNode.eulerAngles.x + (-Float.pi / 2), paintingNode.eulerAngles.y, paintingNode.eulerAngles.z)
+        paintingNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(paintingNode)
+        grid.removeFromParentNode()
+    }
+    
+    // Helper functions not to sure what is happening here
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        let grid = Grid(anchor: planeAnchor)
+        self.grids.append(grid)
+        node.addChildNode(grid)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else { return }
+        let grid = self.grids.filter { grid in
+            return grid.anchor.identifier == planeAnchor.identifier
+            }.first
+        
+        guard let foundGrid = grid else {
+            return
+        }
+        
+        foundGrid.update(anchor: planeAnchor)
     }
     
 
