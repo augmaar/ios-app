927d8f85e73351084ef45ca8da29e1dfe034d3ee
Chase Carnaroli <chasecarnaroli@gmail.com>
2019-03-13 16:55:31 -0700

    Drafted basic profile page

--- Augma/Augma/View Controllers/ProfileController.swift
+++ Augma/Augma/View Controllers/ProfileController.swift
@@ -7,16 +7,70 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ProfileController: UIViewController {
+class ProfileController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var artwork = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4 // Space in between ROWS
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 2
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                self.artwork = dataDictionary["results"] as! [[String:Any]]
+                
+                self.collectionView.reloadData()
+                
+            }
+        }
+        task.resume()
         // Do any additional setup after loading the view.
     }
     
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return artwork.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Cell.PictureGridCell.rawValue, for: indexPath) as! PictureGridCell
+        
+        let movie = artwork[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.pictureView.af_setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
